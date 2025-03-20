0b28a02aaf54217129003cf99a62f8f21c4554d7
sayudha <mazaya.studio@outlook.com>
2025-03-20 09:34:54 +0700

    add error handling to joke fetching in Joke component

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -10,8 +10,17 @@ const Joke = () => {
   const fetchAPI = () => {
     setLoading(true);
     fetch('https://sv443.net/jokeapi/v2/joke/Programming?type=single')
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
       .then((data) => setJoke(data.joke))
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+        setJoke('Failed to fetch joke');
+      })
       .finally(() => setLoading(false));
   };
 
