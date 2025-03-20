2ea13cf683a547604826ada6f0714e63bf64cffc
sayudha <mazaya.studio@outlook.com>
2025-03-20 08:55:26 +0700

    update App component to include a title for the joke generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <h1>Joke generator using React and Joke API</h1>
+    </div>
+  );
 }
